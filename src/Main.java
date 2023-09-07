cae3ff5f6b62ffe414a945c3060f8205029aa5b8
AdrienDiaz <matteoadrien126@gmail.com>
2023-09-07 11:36:31 +0800

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("My Name is Adrien Diaz from Laguna Calamba");
+            System.out.println("My Name is Adrien Diaz from Laguna1");
         }
     }
