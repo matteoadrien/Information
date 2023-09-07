3244151f944c836273d1aa460c1a4fc0b2abc88c
SimpleLearn Github <144195744+matteoadrien@users.noreply.github.com>
2023-09-07 10:15:06 +0800

    Java1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("My Name is Adrien Diaz");
+            System.out.println("My Name is Adrien Diaz from Laguna");
         }
     }
